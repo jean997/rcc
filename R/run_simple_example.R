004e0ebf6957bb440eab0539eda46ba1754b8e94
Jean Morrison <jeanm5@uw.edu>
2015-12-30 18:13:17 -0800

    Add selectiveInference method

--- R/run_simple_example.R
+++ R/run_simple_example.R
@@ -3,7 +3,8 @@ run_simple_example <- function(){
   set.seed(6587900)
   sim.list <- list()
   for(i in 1:4){
-    sim.list[[i]] <- example_sim(DELTA[,i], n=200, use.abs=FALSE)
+    sim.list[[i]] <- example_sim(example_params[,i], n=200, use.abs=TRUE)
   }
-  save(sim.list, file="data/example_sims.RData")
+  return(sim.list)
+  #save(sim.list, file="data/example_sims.RData")
 }
