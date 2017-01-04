ee82946d90d7910a9e9d2fb910f11af33b7709b2
Jean Morrison <jeanm@uchicago.edu>
2017-01-04 12:26:46 -0600

    use nonpar_bs_ci

--- R/linreg_sim.R
+++ R/linreg_sim.R
@@ -16,12 +16,20 @@
 #'  \item{\code{simnames}}{Name of each type of the 5 types of confidence intervals}
 #' }
 #'@export
-linreg_sim <- function(X.pop, which.sample, n.rep=1,
-                      index= c(), beta=c(0), seed=NULL){
+linreg_sim <- function(X.pop, which.sample=1:100, n.rep=1,
+                      index= c(), beta=c(0), seed=NULL, parallel=FALSE){
   if(!is.null(seed)) set.seed(seed)
 	n <-length(which.sample)
   p <- dim(X.pop)[2]
   X.sample <- X.pop[which.sample, ]
+  
+  lr_func <- function(data){
+    y <- data[,1]
+    X <- data[, -1]
+    ests <- rcc:::many_lr(y, X, parallel=FALSE)
+    df <- data.frame("estimate"=ests$beta_hat, "se"=ests$se_hat, "statistic"=ests$beta_hat/ests$se_hat)
+    return(df)
+  }
  
   simnames <- c("nonpar", "par", "wfb", "wfb2", "naive", "selInf1", "ash")
 	COVERAGE <- array(dim=c(length(simnames), p, n.rep))
@@ -32,29 +40,30 @@ linreg_sim <- function(X.pop, which.sample, n.rep=1,
     #Calculate population effects
     z <- lr_pheno_effects_population(X.pop=X.pop, index=index,
                                      beta=beta, sd.err=1,
-                                     which.sample=which.sample)
+                                     which.sample=which.sample, parallel=parallel)
     y <- z$y
     effects <- z$effects
 
     #Run linear regressions for each collumn of X.sample
-		my.bhat <-  many_lr(y, X.sample)
-		j <- order(abs(my.bhat$beta_hat/my.bhat$se_hat), decreasing=TRUE)
-
+    data <- cbind(y, X.sample)
+		res.orig <- lr_func(data)
+		j <- order(abs(res.orig$statistic), decreasing=TRUE)
+    rank <- match(1:p, j)
 		cat("Got effect sizes.\n")
 
 		#Non parametric bootstrap
-		ci.nonpar <- lr_bs_nonpar_ci(y,X.sample,my.bhat$beta_hat,my.bhat$se_hat,n.rep=1000)
+		ci.nonpar <- nonpar_bs_ci(data, analysis.func = lr_func,n.rep=1000, res.orig=res.orig, level = 0.9, parallel=parallel)
 		ci.nonpar <- ci.nonpar$ci
 		COVERAGE[which(simnames=="nonpar"), ,i] <- (ci.nonpar[,1] < effects & effects < ci.nonpar[,2])[j]
 		WIDTH[which(simnames=="nonpar"), , i] <- (ci.nonpar[,2] -ci.nonpar[,1])[j]
 
 		#Parametric bootstrap
-		ci.par <- par_bs_ci(my.bhat$beta_hat,my.bhat$se_hat,n=1000, level=0.1)
+		ci.par <- par_bs_ci(res.orig$estimate, res.orig$se,n.rep=1000, level=0.1)
 		COVERAGE[which(simnames=="par"), ,i] <- (ci.par[,1] < effects & effects < ci.par[,2])[j]
 		WIDTH[which(simnames=="par"), , i] <- (ci.par[,2] -ci.par[,1])[j]
 
 		#WFB CIs conditional on taking the top 10%
-		t <- my.bhat$beta_hat/my.bhat$se_hat
+		t <- res.orig$estimate/res.orig$se
 		ct <- abs(t)[j][floor(0.1*p) + 1]
 		wfb <- lapply(t, FUN=function(x){
 			if(abs(x) < ct) return(c(NA, NA))
@@ -63,8 +72,8 @@ linreg_sim <- function(X.pop, which.sample, n.rep=1,
 			return(ci)
 			})
 		ci.wfb <- matrix(unlist(wfb), byrow=TRUE, nrow=p)
-		ci.wfb[,1] <- ci.wfb[,1]*my.bhat$se_hat
-		ci.wfb[,2] <- ci.wfb[,2]*my.bhat$se_hat
+		ci.wfb[,1] <- ci.wfb[,1]*res.orig$se
+		ci.wfb[,2] <- ci.wfb[,2]*res.orig$se
 		COVERAGE[which(simnames=="wfb"), ,i] <- (ci.wfb[,1] < effects & effects < ci.wfb[,2])[j]
 		WIDTH[which(simnames=="wfb"), , i] <- (ci.wfb[,2] -ci.wfb[,1])[j]
 
@@ -78,14 +87,14 @@ linreg_sim <- function(X.pop, which.sample, n.rep=1,
 			return(unlist(ci))
 			})
 		ci.wfb.2 <- matrix(unlist(wfb.2), byrow=TRUE, nrow=p)
-		ci.wfb.2[,1] <- ci.wfb.2[,1]*my.bhat$se_hat
-		ci.wfb.2[,2] <- ci.wfb.2[,2]*my.bhat$se_hat
+		ci.wfb.2[,1] <- ci.wfb.2[,1]*res.orig$se
+		ci.wfb.2[,2] <- ci.wfb.2[,2]*res.orig$se
 		COVERAGE[which(simnames=="wfb2"),,i] <- (ci.wfb.2[,1] < effects & effects < ci.wfb.2[,2])[j]
 		WIDTH[which(simnames=="wfb2"), , i] <- (ci.wfb.2[,2] -ci.wfb.2[,1])[j]
 
 		#Naive ci
-		ci.naive <- cbind(my.bhat$beta_hat - my.bhat$se_hat*qt(0.95, df=p-1),
-		                  my.bhat$beta_hat + my.bhat$se_hat*qt(0.95, df=p-1))
+		ci.naive <- cbind(res.orig$estimate - res.orig$se*qt(0.95, df=p-1),
+		                  res.orig$estimate + res.orig$se*qt(0.95, df=p-1))
 		COVERAGE[which(simnames=="naive"), ,i] <- (ci.naive[,1] < effects & effects < ci.naive[,2])[j]
 		WIDTH[which(simnames=="naive"), , i] <- (ci.naive[,2] -ci.naive[,1])[j]
 
@@ -93,15 +102,15 @@ linreg_sim <- function(X.pop, which.sample, n.rep=1,
 		M <- manyMeans(y=t, k=0.1*p, alpha=0.1, sigma=1)
 		ci.rtt1 <- matrix(nrow=p, ncol=2)
 		ci.rtt1[M$selected.set, ] <- M$ci
-		ci.rtt1[,1] <- ci.rtt1[,1]*my.bhat$se_hat
-		ci.rtt1[,2] <- ci.rtt1[,2]*my.bhat$se_hat
+		ci.rtt1[,1] <- ci.rtt1[,1]*res.orig$se
+		ci.rtt1[,2] <- ci.rtt1[,2]*res.orig$se
 		COVERAGE[simnames == "selInf1", ,i]<- (ci.rtt1[,1] < effects & effects < ci.rtt1[,2])[j]
 		WIDTH[simnames=="selInf1", , i] <- (ci.rtt1[, 2] - ci.rtt1[,1])[j]
 
 		
 		#ashr credible intervals
-		ash.res <- ash(betahat = my.bhat$beta_hat, sebetahat = my.bhat$se_hat, mixcompdist = "normal")
-		ci.ash <- ashci(ash.res, level=0.9, betaindex = 1:500, trace=FALSE)
+		ash.res <- ash(betahat = res.orig$estimate, sebetahat = res.orig$se, mixcompdist = "normal")
+		ci.ash <- ashci(ash.res, level=0.9, betaindex = 1:p, trace=FALSE)
 		COVERAGE[simnames == "ash", ,i]<- (ci.ash[,1] < effects & effects < ci.ash[,2])[j]
 		WIDTH[simnames=="ash", , i] <- (ci.ash[, 2] - ci.ash[,1])[j]
 		
